b6c2e596e6e209208a0d3452825fc4825300b310
x0054849 <x0054849@vulns.gateway.2wire.net>
2013-12-31 00:45:13 -0800

    3rd commit. Integrating into auto_snorter.rb

--- pcapSight.rb
+++ pcapSight.rb
@@ -2,6 +2,11 @@
 require 'packetfu'; require 'rainbow'
 # API dox http://planb-security.net/packetfu/doc/
 
+# TO-DO:
+# Get the src/dst ip parsing issues worked out
+# Integrate into auto_snorter.rb
+# See what else from a malware perspective makes sense to add as new features
+
 # User supplies .pcap as an argument
 pcap  = ARGV[0] || exit
 count = 0
@@ -22,10 +27,19 @@ puts "-" * title.size
 puts title
 puts "-" * title.size
 
-puts "#{count}".foreground(:magenta).bright + " packets".rjust(12).foreground(:magenta).bright
+puts "#{count}".foreground(:magenta).bright + " packets".rjust(12).foreground(:magenta).bright # inop / fix
 
 # Read file packets
 r = PacketFu::PcapFile.read(pcap)
 
 # Print packet size to user
-puts "#{r}".size.to_s.foreground(:magenta).bright + " bytes\n".rjust(9).foreground(:magenta).bright
+puts "#{r}".size.to_s.foreground(:magenta).bright + " bytes\n".rjust(9).foreground(:magenta).bright # inop / fix
+
+# Read Source IP's
+#src = PacketFu::IPHeader.ip_saddr(pcap)
+
+# Read Destination IP's
+#dst = PacketFu::IPHeader.ip_daddr(pcap)
+
+#puts src
+#puts dst
